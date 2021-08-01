f75644d60470d0565328de9580da855344e22097
harisfi <acc.cp.haris@gmail.com>
2021-08-02 00:40:01 +0700

    Update home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,24 @@
-import './Home.css';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import Hero from '../../components/Hero/Hero';
+import ProductList from '../../components/ProductList/ProductList';
 
-
+import './Home.css';
 
 function Home() {
   return (
-    
     <div className="Home">
-      <p>Home</p>
-      <FontAwesomeIcon icon="check-square" /> <br></br>
-      <FontAwesomeIcon icon={['fab', 'apple']} />
+      <Hero />
+      <div className="d-flex justify-content-between mt-5 mb-3">
+        <div className="h2">Best <strong>Products</strong></div>
+        <div className="d-block">
+          <Link to="/products" className="btn btn-success">
+            <FontAwesomeIcon icon={['far', 'eye']} /> Lihat Semua
+          </Link>
+        </div>
+      </div>
+      <ProductList isHome={true} />
     </div>
   );
 }
