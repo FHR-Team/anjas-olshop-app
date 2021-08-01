ada8144d2c63a8e10ddbdd215070e70b4b4586b8
farelamo <farizfahmi674@gmail.com>
2021-08-01 21:18:51 +0700

    add fitur fontawasome

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import 'bootstrap/scss/bootstrap.scss';
 
 ReactDOM.render(
@@ -15,5 +17,6 @@ ReactDOM.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-
+library.add(fab, faCheckSquare, faCoffee)
 reportWebVitals();
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,16 @@
 import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 
 function Home() {
   return (
+    
     <div className="Home">
       <p>Home</p>
+      <FontAwesomeIcon icon="check-square" /> <br></br>
+      <FontAwesomeIcon icon={['fab', 'apple']} />
     </div>
   );
 }
