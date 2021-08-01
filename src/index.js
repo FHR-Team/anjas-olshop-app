e4909b8f5a5e224d75750c90f8d04384ffdeb124
harisfi <acc.cp.haris@gmail.com>
2021-08-01 22:41:09 +0700

    Update fontawesome initialization

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
 import 'bootstrap/scss/bootstrap.scss';
+
+library.add(fab, fas, far)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,5 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-
-library.add(fab, faCheckSquare, faCoffee)
 reportWebVitals();
