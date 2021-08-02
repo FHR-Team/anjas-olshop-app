ebb5eec04eac78d03497a03a57db235ef78cd89e
harisfi <acc.cp.haris@gmail.com>
2021-08-02 10:07:59 +0700

    Update products page

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom';
+import ProductList from '../../components/ProductList/ProductList';
 import './Products.css';
 
-function Products() {
+function Products(props) {
   return (
     <div className="Products">
-      <p>Products</p>
+      <h1 className="mt-4 mb-1">Daftar <strong>Produk</strong></h1>
+      <nav aria-label="breadcrumb" className="mb-4">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">Products</li>
+        </ol>
+      </nav>
+      <ProductList products={props.products}/>
     </div>
   );
 }
